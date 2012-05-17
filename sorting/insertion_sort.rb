8f15cf2eed315de3ca55735debae6f40bd5fdc82
Hiroyuki Inoue <mammymax@gmail.com>
2012-05-17 16:06:34 +0900

    add insertion_sort

---
+++ sorting/insertion_sort.rb
@@ -0,0 +1,17 @@
+require './util.rb'
+require 'pp'
+
+def insertion_sort( array )
+  for i in 0..( array.length - 1 )
+    j = i
+    while j > 0 && ( array[j-1] > array[j] )
+      array.swap( j-1, j )
+      j = j - 1
+    end
+  end
+  array
+end
+
+rand_array = create_rand_array
+pp rand_array
+pp insertion_sort rand_array
